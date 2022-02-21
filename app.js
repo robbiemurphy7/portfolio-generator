5cebea709cccb47d78785e01464caf9a44bb33f1
robbiemurphy <rmurphy987@gmail.com>
2022-02-20 20:10:25 -0600

    generate webpage

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const FileSystem = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-    console.log('================');
+const [name, github] = profileDataArgs;
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+FileSystem.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
